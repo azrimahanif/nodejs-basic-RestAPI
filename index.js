738eb8636c42169e6e349335eede0ac253a70cc4
azrimahanif <azrimahanif@gmail.com>
2023-10-03 21:23:50 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import apiRoutes from "./routes/index.js";
+import { dbInit } from "./database/connection.js";
+import cors from "cors";
+
+const app = express();
+const PORT = 8080;
+
+dbInit();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// declare routes
+app.use(apiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
